bde4b54e99ddcfef80bf183c167a208af5a2a470
carrotcake218 <143497860+carrotcake218@users.noreply.github.com>
2024-05-21 17:05:17 +0900

    commitbeforesubway

--- src/Layout/style.js
+++ src/Layout/style.js
@@ -19,14 +19,6 @@ export const Header = styled.header`
   backdrop-filter: blur(5px); // 배경 블러 처리
 `;
 
-// 푸터 스타일
-export const Footer = styled.footer`
-  position: fixed; // 스크롤 시에도 하단에 고정
-  bottom: 0; // 하단에 위치
-  left: 0; // 왼쪽에 위치
-  width: 100%; // 전체 너비 차지
-`;
-
 // 네비게이션 바 스타일
 export const NavBar = styled.nav`
   display: flex; // Flexbox 레이아웃 사용
@@ -73,7 +65,8 @@ export const Main = styled.main`
   height: auto; // 자동 높이 설정
   display: flex; // Flexbox 레이아웃 사용
   flex-direction: column; // 세로 방향으로 정렬
-
+  justify-content: center; // 수직 가운데 정렬
+  align-items: center; // 수평 가운데 정렬
   flex: 1; // 나머지 공간을 모두 차지하도록 설정
   padding: 20px; // 내용과 경계 사이에 간격 추가
 `;
